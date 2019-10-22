8a7477e755f8b971ee6b5d376ce92e8e53597b4b
Nayara Ferreira <nayara.ferreira@tron.com.br>
2019-10-22 18:07:38 -0300

    20191022#02: inclusao da model e controller do endereco

---
+++ src/models/Address.js
@@ -0,0 +1,16 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Address extends Model {
+    static init(sequelize) {
+        super.init({
+            zipcode: DataTypes.STRING,
+            street: DataTypes.STRING,
+            number: DataTypes.STRING,
+        },
+        {
+            sequelize
+        })
+    }
+}
+
+module.exports = Address;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
-const ExercisesController = require('../controllers/ExercisesController');
+const AddressController = require('../controllers/AddressController');
+//const ExercisesController = require('../controllers/ExercisesController');
 
 const routes = express.Router();
 
@@ -11,8 +12,11 @@ routes.get('/', (req, res) => {
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
+routes.post('/addresses', )
 
-routes.get('/exercises', ExercisesController.index);
-routes.post('/exercises', ExercisesController.store);
+//routes.get('/exercises', ExercisesController.index);
+//routes.post('/exercises', ExercisesController.store);
 
 module.exports = routes;
+
+//verificar o uso do consign ou require-directory
