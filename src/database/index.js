4a78689a345402604643f52655d12edea18c3182
Nay Ferreira <ncy.fers@gmail.com>
2019-10-23 19:00:41 -0300

    20191023#03: criado metodo de inicializacao do model de exercicios

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize');
 const dbConfig  = require('../config/database');
 
 const User = require('../models/User');
+const Exercises = require('../models/Exercises');
 
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
+Exercises.init(connection);
 
 module.exports = connection;
