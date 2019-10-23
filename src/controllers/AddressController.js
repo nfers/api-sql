b70e15ee8f81965cc2c120d995bbe25d25e2f56e
Nay Ferreira <ncy.fers@gmail.com>
2019-10-23 19:52:38 -0300

    20191023#03: ajuste nos controles - método de busca

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -1,6 +1,21 @@
 const Address = require('../models/Address');
-module.exports = {
-    async StorageEvent(req, res) {
 
+module.exports = {
+    async store(req, res) {
+        const { 
+            zipcode, 
+            street, 
+            number 
+        } = req.body;
+
+        const local = await Address.create({
+            zipcode, 
+            street, 
+            number  
+        });
+
+        return res.json(local)
     }
-}
+};
+
+
--- src/controllers/ExercisesController.js
+++ src/controllers/ExercisesController.js
@@ -6,16 +6,12 @@ module.exports = {
 
         return res.json(exerc);
     }, 
-
     async store(req, res) {
         const { description, limbs } = req.body;
 
         const exercise = await Excercises.create({ 
             description, limbs
         });
-
         return res.json(exercise);
-
-
     }
 }
