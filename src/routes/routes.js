d16891e4bbf75b9011b92e826e5b2290940ee4d7
Nay Ferreira <ncy.fers@gmail.com>
2019-10-24 12:27:19 -0300

    20191023#002: inclusao das rotas de get e post para o model de dados corporais

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const UserController = require('../controllers/UserController');
 const AddressController = require('../controllers/AddressController');
 const ExercisesController = require('../controllers/ExercisesController');
 const ReportController = require('../controllers/ReportController');
-
+const BodyDataController = require('../controllers/BodyDataController');
 
 const routes = express.Router();
 
@@ -14,6 +15,10 @@ routes.get('/exercises', ExercisesController.index);
 routes.post('/exercises', ExercisesController.store);
 routes.get('/exercises/:id', ExercisesController.findOne);
 routes.put('/exercises/:id', ExercisesController.update);
+
+routes.get('/users/:user_id/bodydata', BodyDataController.index);
+routes.post('/users/:user_id/bodydata', BodyDataController.store);
+
 
 routes.get('/users/:user_id/addresses', AddressController.index);
 routes.post('/users/:user_id/addresses', AddressController.store);
